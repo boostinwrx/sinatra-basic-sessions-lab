ffa8f18d01f3054d34ae14a29dd91bbd026b85df
boostinwrx <tony.calablava@gmail.com>
2020-01-08 02:38:35 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,19 +1,16 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
-
   configure do
     enable :sessions
     set :session_secret, 'tiff'
   end
-
   get '/' do
     erb :index
   end
 
   post '/checkout' do
     session[:item] = params[:item]
-    @session = session
+    @shopping_session = shopping_session
     erb :checkout
   end
 end
